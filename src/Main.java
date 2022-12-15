a4b3c9be8c71e1050620929cbaadb56542b5391f
Davran2005 <davranjoldoshbaev2@gmail.com>
2022-12-15 15:59:49 +0600

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+         Integer[] num={1,4,6,9,11,8,7,6,5,12,2,3};
+        Stream<Integer>number=Stream.of(num);
+        Stream<Integer>stream=number.filter(x -> x % 2==0).map(x -> x*x);
+        stream.max(Comparator.comparing(x->x)).stream().toList().forEach(System.out::println);
     }
 }
